e4ddb93a606d49126eafdd1e282de288d4a464ee
Damir-k <damir@t1c.ru>
2020-09-18 19:44:24 +0300

    Completely new code (much better!)

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
-import os
 import discord
+from discord.ext import commands
+import config
+import random
 
-TOKEN = input("Insert Bot Token here: ")
-
-client = discord.Client()
+client = commands.Bot(command_prefix=">")
 
 @client.event
 async def on_ready():
-    print(client.user, "Has connected!")
+    print('Logged in!')
 
+@client.command()
+async def wow(ctx):
+    await ctx.send(":clown:, что сказать")
 
-client.run(TOKEN)
+@client.command()
+async def ping(ctx):
+    await ctx.send(f"bot ping: {round(client.latency * 1000)}ms")
+
+@client.command()
+async def coinflip(ctx):
+    result = random.choice(["Решка", "Орел"])
+    await ctx.send(result)
+
+client.run(config.TOKEN)
