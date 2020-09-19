9170f13826025d08085f7c951eeb63d9317bcfeb
Damir-k <damir@t1c.ru>
2020-09-19 15:44:22 +0300

    heroku support

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ client.remove_command("help")
 #  initialization
 @client.event
 async def on_ready():
+    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="за джастиком"))
     print('Logged in!')
 
 #
