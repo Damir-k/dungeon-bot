7660bbc1b4faf511d6ffd78a9a07490b2cda8d41
Damir-k <damir@t1c.ru>
2020-09-20 09:47:58 +0300

    load is now not unloadable

--- cogs/DevOnly.py
+++ cogs/DevOnly.py
@@ -1,25 +1,19 @@
 import discord
 from discord.ext import commands
 
-DEVELOPERS = [
-    433668397599948810, #  HRODGRIM
-    357079203235233792  #  Eugene
-]
+def is_developer(ctx):
+
+    DEVELOPERS = [
+        433668397599948810, #  HRODGRIM
+        357079203235233792  #  Eugene
+    ]
+
+    return ctx.author.id in DEVELOPERS
 
 class DevOnly(commands.Cog):
 
     def __init__(self, client):
         self.client = client
-    
-    @staticmethod
-    def is_developer(ctx):
-        return ctx.author.id in DEVELOPERS
-
-    @commands.command()
-    @commands.check(is_developer)
-    async def load(self, ctx, extention):
-        self.client.load_extension(f"cogs.{extention}")
-        await ctx.send(f"Модуль {extention} успешно загружен")
 
     @commands.command()
     @commands.check(is_developer)
--- optimized_bot.py
+++ optimized_bot.py
@@ -5,6 +5,21 @@ from discord.ext import commands
 
 client = commands.Bot(command_prefix=">")
 
+def is_developer(ctx):
+
+    DEVELOPERS = [
+        433668397599948810, #  HRODGRIM
+        357079203235233792  #  Eugene
+    ]
+
+    return ctx.author.id in DEVELOPERS
+
+@client.command()
+@commands.check(is_developer)
+async def load(self, ctx, extention):
+    self.client.load_extension(f"cogs.{extention}")
+    await ctx.send(f"Модуль {extention} успешно загружен")
+
 for filename in os.listdir("./cogs"):
     if filename.endswith(".py"):
         client.load_extension(f"cogs.{filename[:-3]}")
